67fcd630a5947d70a661727e60b030d8e99b99a6
Danny Kirschner <rkirschner1377@gmail.com>
2013-11-16 12:51:38 -0500

    adding default mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,15 @@ Rhodeguide::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => "rhodeguide.herouapp.com" }
+  ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
